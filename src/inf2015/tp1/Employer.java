45be16e4e6f5b66740afdf307359e9d5747be211
fdufault <fdufault@10.0.0.2>
2012-10-18 20:51:38 -0400

    TP Agile Simple Creation

---
+++ src/inf2015/tp1/Employer.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inf2015.tp1;
+
+/**
+ *
+ * @author fdufault
+ */
+public class Employer {
+    private 
+    
+    
+}
---
+++ src/inf2015/tp1/TPAgileSimple.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inf2015.tp1;
+
+/**
+ *
+ * @author fdufault
+ */
+public class TPAgileSimple {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+    }
+}
