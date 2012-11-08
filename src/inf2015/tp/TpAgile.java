47a49fa3a2b8b8887cecdb6e19968f4436693c13
fdufo <fdufo@172.29.91.246>
2012-11-08 18:49:30 -0500

    Refactoring: application du style

---
+++ src/inf2015/tp/TpAgile.java
@@ -0,0 +1,28 @@
+/**
+ * TPAgile - INF2015 - TP Agile - EQUIPE 17
+ *
+ * @author Francois Dufault
+ * @author Lyes Tamazouzt
+ * @author Abdessamad Essakhi
+ */
+package inf2015.tp;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class TpAgile {
+
+    public static void main(String[] args) {
+        Employe employer = new Employe();
+
+        try {
+            employer.chargerFeuillerTemps(args[0]);
+            employer.calculerFeuilleTemps();
+            employer.analyserFeuilleTemps();
+            ErreurLog.Instance().ecrireErreur(args[1]);
+        } catch (IOException ex) {
+            Logger.getLogger(TpAgile.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
