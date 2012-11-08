4d8b52a668a98042b3dc873946af72ad1160169f
fdufo <fdufo@172.29.91.246>
2012-11-08 18:21:54 -0500

    Refactoring: Application du style à la classe

--- src/inf2015/tp/ErreurLog.java
+++ src/inf2015/tp/ErreurLog.java
@@ -1,51 +1,40 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * ErreurLog - INF2015 - TP Agile - EQUIPE 17
+ *
+ * @author Francois Dufault
+ * @author Lyes Tamazouzt
+ * @author Abdessamad Essakhi
  */
 package inf2015.tp;
 
 import java.io.IOException;
 import net.sf.json.JSONArray;
 
-/**
- *
- * @author fdufault
- */
 public class ErreurLog {
+
     private static ErreurLog ERREUR_LOG_INSTANCE = new ErreurLog();
-    
-    private JSONArray _erreurs = new JSONArray();
-    
-    
-    
-    public void ajoutErreur(String aErreurMessage)
-    {
-        this._erreurs.add(aErreurMessage);
+    private JSONArray erreurs = new JSONArray();
+
+    public void ajoutErreur(String aErreurMessage) {
+        this.erreurs.add(aErreurMessage);
         System.out.println(aErreurMessage);
     }
-    
-    public void ecrireErreur(String aCheminFichier) throws IOException
-    {
-        JSONUtil.writeJSONArray(this._erreurs, aCheminFichier);
+
+    public void ecrireErreur(String aCheminFichier) throws IOException {
+        JSONUtil.writeJSONArray(this.erreurs, aCheminFichier);
     }
-    
+
     @Override
-    public String toString()
-    {
+    public String toString() {
         //toString(5) => 5 espaces pour chaque indentation.
-       return this._erreurs.toString(5);
+        return this.erreurs.toString(5);
     }
-    
-    public void effacerTout()
-    {
-        this._erreurs.clear();
+
+    public void effacerTout() {
+        this.erreurs.clear();
     }
-    
-    public static ErreurLog Instance()
-    {
+
+    public static ErreurLog Instance() {
         return ERREUR_LOG_INSTANCE;
     }
-    
-    
-    
 }
