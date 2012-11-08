ea606d92a1fce2ee4615ab247d8047ce90a9d655
fdufo <fdufo@172.29.91.246>
2012-11-08 18:44:40 -0500

    Refactoring: application du style

--- src/inf2015/tp/Jour.java
+++ src/inf2015/tp/Jour.java
@@ -1,31 +1,30 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Jour - INF2015 - TP Agile - EQUIPE 17
+ *
+ * @author Francois Dufault
+ * @author Lyes Tamazouzt
+ * @author Abdessamad Essakhi
  */
 package inf2015.tp;
 
 import java.util.ArrayList;
 
-/**
- *
- * @author fdufault
- */
 public class Jour {
 
     private static final int MINUTES_JOURNEE_FERIEE = 420;
-    private ArrayList<Projet> _projetsJournee = new ArrayList<>();
-    private TypeJour _typeJournee;
-    private String _nomJour;
+    private ArrayList<Projet> projetsJournee = new ArrayList<>();
+    private TypeJour typeJournee;
+    private String nomJour;
 
-    private Jour(String aNomJour, TypeJour aTypeJournee) {
-        this._typeJournee = aTypeJournee;
-        this._nomJour = aNomJour;
+    private Jour(String nomJour, TypeJour typeJournee) {
+        this.typeJournee = typeJournee;
+        this.nomJour = nomJour;
     }
 
     public boolean estJourneeFerie() {
         boolean estFerie = false;
 
-        for (Projet projet : this._projetsJournee) {
+        for (Projet projet : this.projetsJournee) {
             if (projet.estCongeFerie()) {
                 estFerie = true;
             }
@@ -37,7 +36,7 @@ public class Jour {
     public boolean contientTeleTravail() {
         boolean estTeleTravail = false;
 
-        for (Projet projet : this._projetsJournee) {
+        for (Projet projet : this.projetsJournee) {
             if (projet.estTeleTravail()) {
                 estTeleTravail = true;
             }
@@ -49,7 +48,7 @@ public class Jour {
     public boolean contientTravailBureau() {
         boolean estTravailBureau = false;
 
-        for (Projet projet : this._projetsJournee) {
+        for (Projet projet : this.projetsJournee) {
             if (projet.estTravailBureau()) {
                 estTravailBureau = true;
             }
@@ -62,7 +61,7 @@ public class Jour {
     public int getMinutesJourneeFeriee() {
         int minutes = 0;
 
-        for (Projet projet : this._projetsJournee) {
+        for (Projet projet : this.projetsJournee) {
             if (projet.estCongeFerie()) {
                 minutes += projet.getMinutes();
             }
@@ -71,35 +70,32 @@ public class Jour {
         return minutes;
     }
 
-    public int getMinutesTeletravail()
-    {
+    public int getMinutesTeletravail() {
         int minutes = 0;
-        
-        for(Projet projet : this._projetsJournee)
-        {
-            if(projet.estTeleTravail())
-            {
+
+        for (Projet projet : this.projetsJournee) {
+            if (projet.estTeleTravail()) {
                 minutes = minutes + projet.getMinutes();
             }
         }
-        
+
         return minutes;
     }
-    
+
     public int getMinutesBureau() {
         int minutes = 0;
 
-        for (Projet projet : this._projetsJournee) {
+        for (Projet projet : this.projetsJournee) {
             if (projet.estTravailBureau() || projet.estCongeFerie()) {
                 minutes = minutes + projet.getMinutes();
             }
         }
-        
+
         return minutes;
     }
-    
-    public void ajoutProjet(Projet aNouveauProjet) {
-        this._projetsJournee.add(aNouveauProjet);
+
+    public void ajoutProjet(Projet nouveauProjet) {
+        this.projetsJournee.add(nouveauProjet);
     }
 
     public void analyserJour() {
@@ -108,53 +104,46 @@ public class Jour {
         } else {
             // AJOUTER AUTRE JOUR SPECIAUX
         }
-
-
     }
 
-    private void analyserJourFerie() {
-        if (this._typeJournee == TypeJour.WEEKEND) {
-            ErreurLog.Instance().ajoutErreur("Le jour " + this._nomJour + " qui est fériée, ne doit pas être le weekend.");
-        }
-
-        if (this.contientTravailBureau()) {
-            ErreurLog.Instance().ajoutErreur("Le jour " + this._nomJour + " qui est fériée, ne doit pas contenir de temps au bureau.");
-        }
-
-        if (this.getMinutesJourneeFeriee() != MINUTES_JOURNEE_FERIEE) {
-            ErreurLog.Instance().ajoutErreur("Le jour \"" + this._nomJour + "\" qui est fériée, doit contenir " + MINUTES_JOURNEE_FERIEE + " minutes. (Il contient " + this.getMinutesJourneeFeriee() + " minutes.)");
-        }
-
+    public boolean estJourOuvrable() {
+        return (this.typeJournee == TypeJour.OUVRABLE);
     }
 
-    public boolean estJourOuvrable()
-    {
-        return (this._typeJournee == TypeJour.OUVRABLE);
-    }
-    
     @Override
-    public String toString()
-    {
-        return String.format("%s | Type: %s",this._nomJour, this._typeJournee);
+    public String toString() {
+        return String.format("%s | Type: %s", this.nomJour, this.typeJournee);
     }
-    
+
     public enum TypeJour {
 
         OUVRABLE,
         WEEKEND
     }
 
-    public static Jour CreerJour(String aNomJour) {
+    private void analyserJourFerie() {
+        if (this.typeJournee == TypeJour.WEEKEND) {
+            ErreurLog.Instance().ajoutErreur("Le jour " + this.nomJour + " qui est fériée, ne doit pas être le weekend.");
+        }
+
+        if (this.contientTravailBureau()) {
+            ErreurLog.Instance().ajoutErreur("Le jour " + this.nomJour + " qui est fériée, ne doit pas contenir de temps au bureau.");
+        }
+
+        if (this.getMinutesJourneeFeriee() != MINUTES_JOURNEE_FERIEE) {
+            ErreurLog.Instance().ajoutErreur("Le jour \"" + this.nomJour + "\" qui est fériée, doit contenir " + MINUTES_JOURNEE_FERIEE + " minutes. (Il contient " + this.getMinutesJourneeFeriee() + " minutes.)");
+        }
+    }
+    
+    public static Jour CreerJour(String nomJour) {
         Jour jour;
 
-        if (aNomJour.startsWith("jour")) {
-            jour = new Jour(aNomJour, TypeJour.OUVRABLE);
+        if (nomJour.startsWith("jour")) {
+            jour = new Jour(nomJour, TypeJour.OUVRABLE);
         } else {
-            jour = new Jour(aNomJour, TypeJour.WEEKEND);
+            jour = new Jour(nomJour, TypeJour.WEEKEND);
         }
 
         return jour;
     }
-    
-    
 }
