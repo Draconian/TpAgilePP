b289dbc781f4fcd461675a2ae4b735e2e95968a0
fdufault <fdufault@videotron.ca>
2012-11-11 14:42:08 -0500

    Validation des arguments de la fonction main (v2)

--- src/inf2015/tp/TpAgile.java
+++ src/inf2015/tp/TpAgile.java
@@ -7,25 +7,18 @@
  */
 package inf2015.tp;
 
-import com.apple.eawt.Application;
-
 public class TpAgile {
     
     public static void main(String[] args) {
-        
-        validerProgrammeArguments(args);
-        
-        Employe employer = new Employe(args[0], args[1]);
-        employer.approuverFeuilleDeTemps();
-    }
-    
-    private static void validerProgrammeArguments(String[] args) {
         
         if(args.length != 2)
         {
             System.out.println("Erreur dans les arguments passés au programme: TpAgile [chemin feuille temps] [chemin erreur sortie]");
             System.exit(-1);
-        }        
+        }   
+        
+        Employe employer = new Employe(args[0], args[1]);
+        employer.approuverFeuilleDeTemps();
     }
     
 }
