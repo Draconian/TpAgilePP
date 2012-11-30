fa3da5208edc0e233868e2241ad76d665f65459b
fdufault <fdufault@videotron.ca>
2012-11-30 13:19:47 -0500

    JourOuvrable: ajout des tags @Override pour les méthodes abstraites

--- src/main/java/inf2015/tp/jour/JourOuvrable.java
+++ src/main/java/inf2015/tp/jour/JourOuvrable.java
@@ -16,14 +16,17 @@ public class JourOuvrable extends Jour {
         super(nomJour);
     }
 
+    @Override
     public boolean estJourOuvable() {
         return true;
     }
 
+    @Override
     protected void analyserJourFerie() {
         comparerJourSpecialEtMinutesRequis(this.nomJour, "férié", this.getMinutesJourneeFeriee(), Jour.MINUTES_JOURNEE_FERIEE);
     }
 
+    @Override
     protected void analyserJourMaladie() {
         if (this.contientAutresProjetsQue(Projet.PROJET_ID_CONGE_MALADIE)) {
            ErreurJournal.Instance().ajoutErreur("\nLe jour " + this.nomJour + " a d'autre code de projet dans la même journée.");
@@ -32,10 +35,12 @@ public class JourOuvrable extends Jour {
         comparerJourSpecialEtMinutesRequis(this.nomJour, "maladie", this.getMinutesJourneeMaladie(), Jour.MINUTES_JOURNEE_MALADIE);
     }
 
+    @Override
     protected void analyserJourVacances() {
         comparerJourSpecialEtMinutesRequis(this.nomJour, "vacance", this.getMinutesJourneeVacance(), Jour.MINUTES_JOURNEE_CONGE_VACANCES);
     }
-
+    
+    @Override
     protected void analyserJourParental() {
         if (this.contientAutresProjetsQue(Projet.PROJET_ID_CONGE_PARENTAL)) {
             ErreurJournal.Instance().ajoutErreur(String.format("\nLe jour \"%s\" qui est congé parental ne doit pas avoir d'autre projet dans la même journée.", this.nomJour));
