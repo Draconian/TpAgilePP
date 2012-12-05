01bf80c1274d14ff415dfedfee684ba2266fd6b8
fdufault <fdufault@videotron.ca>
2012-12-05 15:18:22 -0500

    Ajout test JsonFabriqueObjTest pour les différents types d'employé

--- src/test/java/inf2015/tp/JsonFabriqueObjTest.java
+++ src/test/java/inf2015/tp/JsonFabriqueObjTest.java
@@ -9,6 +9,9 @@ package inf2015.tp;
 
 import inf2015.tp.employe.Employe;
 import inf2015.tp.employe.EmployeAdministration;
+import inf2015.tp.employe.EmployeDeveloppement;
+import inf2015.tp.employe.EmployeDirection;
+import inf2015.tp.employe.EmployeExploitation;
 import inf2015.tp.jour.Jour;
 import inf2015.tp.jour.JourOuvrable;
 import inf2015.tp.jour.JourWeekend;
@@ -37,7 +40,7 @@ public class JsonFabriqueObjTest {
     public void testFabriquerJourOuvrable() {
         String nomJour = "jour1";
         JsonFabriqueObj fabrique = new JsonFabriqueObj(null);
-        
+
         JourOuvrable jour = fabrique.fabriquerJourOuvrable(nomJour);
 
         JourOuvrable jourExpecte = new JourOuvrable(nomJour, null);
@@ -48,7 +51,7 @@ public class JsonFabriqueObjTest {
     @Test
     public void testFabriquerJourWeekend() {
         String nomJour = "weekend1";
-JsonFabriqueObj fabrique = new JsonFabriqueObj(null);
+        JsonFabriqueObj fabrique = new JsonFabriqueObj(null);
         JourWeekend jour = fabrique.fabriquerJourWeekend(nomJour);
 
         JourWeekend jourExpecte = new JourWeekend(nomJour, null);
@@ -83,37 +86,80 @@ JsonFabriqueObj fabrique = new JsonFabriqueObj(null);
         assertEquals(projetExpecter.minutes, projetCreer.minutes);
         assertEquals(projetExpecter.projetID, projetCreer.projetID);
     }
-    
+
     @Test
     public void testFabriquerFeuilleTemps1Jour1Projet() throws Exception {
         JsonFabriqueObj fabrique = new JsonFabriqueObj(null);
         String fichierJsonContenu = "{\"numero_employe\": 500,\"jour1\": [ {\"projet\": 800,\"minutes\": 840}], \"jour2\": [], \"jour3\": [], \"jour4\": [], "
                 + "\"jour5\": [], \"weekend1\":[], \"weekend2\": [] }";
         Employe employeCreer = fabrique.fabriquerFeuilleTempsDuTexteJson(fichierJsonContenu);
-        
+
         assertEquals("jour1", employeCreer.getSemaine().get(0).getNomJour());
         assertEquals(500, employeCreer.getNumeroEmploye());
         assertEquals(840, employeCreer.getSemaine().get(0).getProjetsJournee().get(0).minutes);
         assertEquals(800, employeCreer.getSemaine().get(0).getProjetsJournee().get(0).projetID);
     }
-    
+
     @Test
-    public void testFabriquerFeuilleTempsMultiple() throws Exception{
+    public void testFabriquerFeuilleTempsMultiple() throws Exception {
         JsonFabriqueObj fabrique = new JsonFabriqueObj(null);
         String fichierJsonContenu = "{\"numero_employe\": 500,\"jour1\": [ {\"projet\": 800,\"minutes\": 840}], \"jour2\": [], \"jour3\": [], \"jour4\": [], "
                 + "\"jour5\": [], \"weekend1\":[{\"projet\": 800,\"minutes\": 840}, {\"projet\": 500,\"minutes\": 1200}], \"weekend2\": [] }";
         Employe employeCreer = fabrique.fabriquerFeuilleTempsDuTexteJson(fichierJsonContenu);
-        
+
         assertEquals("jour1", employeCreer.getSemaine().get(0).getNomJour());
         assertEquals(500, employeCreer.getNumeroEmploye());
         assertEquals(840, employeCreer.getSemaine().get(0).getProjetsJournee().get(0).minutes);
         assertEquals(800, employeCreer.getSemaine().get(0).getProjetsJournee().get(0).projetID);
         assertEquals(employeCreer.getSemaine().get(0).getClass(), JourOuvrable.class);
-        
-        
+
+
         assertEquals("weekend1", employeCreer.getSemaine().get(5).getNomJour());
         assertEquals(1200, employeCreer.getSemaine().get(5).getProjetsJournee().get(1).minutes);
         assertEquals(500, employeCreer.getSemaine().get(5).getProjetsJournee().get(1).projetID);
         assertEquals(employeCreer.getSemaine().get(5).getClass(), JourWeekend.class);
+    }
+    
+    @Test 
+    public void testFabriquerEmployeSelonNumeroAdministration() throws Exception {
+        JsonFabriqueObj fabrique = new JsonFabriqueObj(null);
+        
+        Employe employe = fabrique.fabriquerEmployeSelonNumero(500);
+        
+        assertEquals(EmployeAdministration.class, employe.getClass());
+    }
+    
+    @Test 
+    public void testFabriquerEmployeSelonNumeroDeveloppement() throws Exception {
+        JsonFabriqueObj fabrique = new JsonFabriqueObj(null);
+        
+        Employe employe = fabrique.fabriquerEmployeSelonNumero(1500);
+        
+        assertEquals(EmployeDeveloppement.class, employe.getClass());
+    }
+    
+    @Test 
+    public void testFabriquerEmployeSelonNumeroExplotation() throws Exception {
+        JsonFabriqueObj fabrique = new JsonFabriqueObj(null);
+        
+        Employe employe = fabrique.fabriquerEmployeSelonNumero(2500);
+        
+        assertEquals(EmployeExploitation.class, employe.getClass());
+    }
+    
+    @Test 
+    public void testFabriquerEmployeSelonNumeroDirection() throws Exception {
+        JsonFabriqueObj fabrique = new JsonFabriqueObj(null);
+        
+        Employe employe = fabrique.fabriquerEmployeSelonNumero(5500);
+        
+        assertEquals(EmployeDirection.class, employe.getClass());
+    }
+    
+    @Test(expected=Exception.class)
+    public void testFabriquerEmployeSelonNumeroInvalide() throws Exception {
+        JsonFabriqueObj fabrique = new JsonFabriqueObj(null);
+        
+        Employe employe = fabrique.fabriquerEmployeSelonNumero(0);
     }
 }
