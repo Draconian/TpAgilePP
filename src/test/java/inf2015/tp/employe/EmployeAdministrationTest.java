0eb6a525ddb62cec077ba6cdf635ff2a8143a1e0
Sonia <Sonia@172.29.107.89>
2012-12-11 16:57:02 -0500

    Test les nouvelles fonctionnalités sur la DDC3

--- src/test/java/inf2015/tp/employe/EmployeAdministrationTest.java
+++ src/test/java/inf2015/tp/employe/EmployeAdministrationTest.java
@@ -14,35 +14,36 @@ import inf2015.tp.erreur.ErreurEmployeMinimalUnJourOuvrableBureau;
 import inf2015.tp.erreur.ErreurEmployeMinimumBureau;
 import inf2015.tp.erreur.ErreurEmployeTeletravailMaximum;
 import inf2015.tp.erreur.ErreurJournal;
+import inf2015.tp.erreur.ErreurTempsMaximaleTransport;
 import inf2015.tp.jour.Jour;
 import inf2015.tp.jour.JourOuvrable;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class EmployeAdministrationTest {
-
+    
     @Test
     public void testGetTypeEmploye() {
         String typeEmploye = "Administration";
         EmployeAdministration employe = new EmployeAdministration(0, null);
-
+        
         assertEquals(typeEmploye, employe.getTypeEmploye());
     }
-
+    
     @Test
     public void testEstEmploye() {
         int employeID = 500;
-
+        
         assertTrue(EmployeAdministration.estEmploye(employeID));
     }
-
+    
     @Test
     public void testEstPasEmploye() {
         int employeID = 1001;
-
+        
         assertFalse(EmployeAdministration.estEmploye(employeID));
     }
-
+    
     @Test
     public void testVerifierMinimumMinutesQuotidienneAdministrationValide() {
         ErreurJournal erreurJournal = new ErreurJournal();
@@ -54,7 +55,7 @@ public class EmployeAdministrationTest {
         employe.verifierMinimumMinutesQuotidiennes();
         assertTrue(erreurJournal.estVide());
     }
-
+    
     @Test
     public void testVerifierMinimumMinutesQuotidienneAdministrationInvalide() {
         ErreurJournal erreurJournal = new ErreurJournal();
@@ -63,82 +64,125 @@ public class EmployeAdministrationTest {
         jour.ajoutProjet(projet);
         Employe employe = new EmployeAdministration(0, erreurJournal);
         employe.ajoutJour(jour);
-
+        
         employe.verifierMinimumMinutesQuotidiennes();
-
+        
         Erreur erreur = erreurJournal.getErreurAIndex(0);
         assertEquals(ErreurEmployeMinimalUnJourOuvrableBureau.class, erreur.getClass());
     }
-
+    
     @Test
     public void testVerifierTeleTravailValide() {
         int minutesTeletravail = 599;
         ErreurJournal erreurJournal = new ErreurJournal();
-
+        
         EmployeAdministration employe = new EmployeAdministration(0, erreurJournal);
         employe.minutesTeleTravail = minutesTeletravail;
         employe.verifierTeleTravail();
-
+        
         assertTrue(erreurJournal.estVide());
     }
-
+    
     @Test
     public void testVerifierTeleTravailInvalide() {
         int minutesTeletravail = 601;
         ErreurJournal erreurJournal = new ErreurJournal();
-
+        
         EmployeAdministration employe = new EmployeAdministration(0, erreurJournal);
         employe.minutesTeleTravail = minutesTeletravail;
         employe.verifierTeleTravail();
-
+        
         assertEquals(1, erreurJournal.getNombresErreurs());
-
+        
         Erreur erreur = erreurJournal.getErreurAIndex(0);
         assertEquals(ErreurEmployeTeletravailMaximum.class, erreur.getClass());
     }
-
+    
     @Test
     public void TestAnalyserFeuilleTempsValide() {
         ErreurJournal erreurJournal = new ErreurJournal();
-
+        
         Employe employe = new EmployeAdministration(0, erreurJournal);
         employe.minutesJoursOuvrableBureau = 2100;
         employe.minutesWeekendBureau = 200;
-
+        
         employe.analyserFeuilleTemps();
-
+        
         assertTrue(erreurJournal.estVide());
     }
-
+    
     @Test
     public void TestAnalyserFeuilleTempsInvalideMinimum() {
         ErreurJournal erreurJournal = new ErreurJournal();
-
+        
         Employe employe = new EmployeAdministration(0, erreurJournal);
         employe.minutesJoursOuvrableBureau = 100;
         employe.minutesWeekendBureau = 200;
-
+        
         employe.analyserFeuilleTemps();
-
+        
         assertEquals(1, erreurJournal.getNombresErreurs());
-
+        
         Erreur erreur = erreurJournal.getErreurAIndex(0);
         assertEquals(ErreurEmployeMinimumBureau.class, erreur.getClass());
     }
-
+    
     @Test
     public void TestAnalyserFeuilleTempsInvalideMaximum() {
         ErreurJournal erreurJournal = new ErreurJournal();
-
+        
         Employe employe = new EmployeAdministration(0, erreurJournal);
         employe.minutesJoursOuvrableBureau = 100;
         employe.minutesWeekendBureau = 2500;
-
+        
         employe.analyserFeuilleTemps();
-
+        
         assertEquals(1, erreurJournal.getNombresErreurs());
-
+        
         Erreur erreur = erreurJournal.getErreurAIndex(0);
         assertEquals(ErreurEmployeMaximumBureau.class, erreur.getClass());
     }
+
+    @Test
+    public void TestMaximumTempsTransport() {
+        ErreurJournal erreurJournal = new ErreurJournal();
+        
+        Employe employe = new EmployeAdministration(500, erreurJournal);
+        int minuteInvalide = 400;
+        
+        employe.validerMinutesTransport(minuteInvalide);
+        Erreur erreur = erreurJournal.getErreurAIndex(0);
+        assertEquals(ErreurTempsMaximaleTransport.class, erreur.getClass());
+    }
+
+    @Test
+    public void TestValideTempsTransport() {
+        ErreurJournal erreurJournal = new ErreurJournal();
+        
+        Employe employe = new EmployeAdministration(500, erreurJournal);
+        int minuteInvalide = 200;
+        
+        employe.validerMinutesTransport(minuteInvalide);
+        
+        assertEquals(erreurJournal.getNombresErreurs(), 0);
+    }
+     @Test
+    public void testVerifierAjoutMinutesBureauTransport() {
+        ErreurJournal erreurJournal = new ErreurJournal();
+       int minutes=200;
+        Employe employe = new EmployeAdministration(500, erreurJournal);   
+        employe.minutesJoursOuvrableBureau = 200;
+        employe.ajusterLesMinutes(200);
+        assertEquals(employe.minutesJoursOuvrableBureau,400);
+    }
+          @Test
+    public void testVerifierPasAjouterMinutesBureauTransport() {
+        ErreurJournal erreurJournal = new ErreurJournal();
+       int minutes=200;
+        Employe employe = new EmployeAdministration(500, erreurJournal);   
+        employe.minutesJoursOuvrableBureau = 200;
+        employe.ajusterLesMinutes(302);
+        assertEquals(employe.minutesJoursOuvrableBureau,200);
+    }
+      
 }
