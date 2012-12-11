ebc8976d40df2c765057f2eebc550145da4bde67
Sonia <Sonia@172.29.107.89>
2012-12-11 16:56:39 -0500

    sa me faisait une erreur a cause du % de toute facon on met une constante dans le message changer String format pour des +

--- src/main/java/inf2015/tp/erreur/ErreurTempsMaximaleTransport.java
+++ src/main/java/inf2015/tp/erreur/ErreurTempsMaximaleTransport.java
@@ -11,9 +11,10 @@ import inf2015.tp.employe.Employe;
  * @author Lyes
  */
 public class ErreurTempsMaximaleTransport extends Erreur{
+  protected static final int NB_HEURES_MAX=5;
   protected static final String ERREUR_MESSAGE = "L'employé %s a dépassé le nombre "
-            + "d'heures de transport. Il ne peut pas dépasser %.1f heures dans la semaine.";
-    private int maxMinutes;
+            + "d'heures de transport. Il ne peut pas dépasser " + NB_HEURES_MAX + " heures dans la semaine.";
+    private int maxMinutes=0;
     
    public ErreurTempsMaximaleTransport(Employe employeErreur, int maxMinutes) {
         super(ERREUR_MESSAGE, employeErreur);
